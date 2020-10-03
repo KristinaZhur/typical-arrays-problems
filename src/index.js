ea248f435054300a2677a3284d68add5597743ac
Kristina Zhuravleva <krist-y_92@mail.ru>
2020-10-03 15:26:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if ( array == null|| array.length == 0 ){
+    return 0;
+  };
+  let min = 0;
+
+  for (let i = 0; i<array.length; i++){
+    if (array[i]<=min){
+      min = array[i];
+    };
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+  let max =0;
+  if ( array == null || array.length == 0 ){
+    return 0;
+  };
+  for (let i = 0; i<array.length; i++){
+    if (array[i]>max){
+      max = array[i];
+    };
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if ( array == null || array.length == 0){
+    return 0;
+  };
+  let summ = 0;
+  let avg =0;
+  for (let i = 0; i<array.length; i++){
+    summ+=array[i];
+  };
+  avg = summ/array.length;
+  return avg;
 }
